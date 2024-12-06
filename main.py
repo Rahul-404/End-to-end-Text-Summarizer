e2b93233d0f2f5a5cc91bde68b206294989d3813
Rahul-404 <rahulshelke981@gmail.com>
2024-12-07 00:47:06 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPiepline
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPiepline
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -7,6 +8,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionPiepline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -1,8 +1,8 @@
 from src.textSummarizer.config.configuration import ConfigurationManager
-from src.textSummarizer.components.data_ingestion import DataIngestion
+from src.textSummarizer.components.data_validation import DataValidation
 from src.textSummarizer.logging import logger
 
-class DataIngestionPiepline:
+class DataValidationPipeline:
 
     def __init__(self):
         pass
@@ -10,19 +10,18 @@ class DataIngestionPiepline:
     def main(self):
         try:
             config = ConfigurationManager()
-            data_ingestion_config = config.get_data_ingestion_config()
-            data_ingestion = DataIngestion(config=data_ingestion_config)
-            data_ingestion.download_file()
-            data_ingestion.extract_zip_file()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValidation(data_validation_config)
+            data_validation.validate_all_files_exist()
         except Exception as e:
             raise e
-        
-STAGE_NAME = "Data Ingestion Stage"
+
+STAGE_NAME = "Data Validation Stage"
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionPiepline()
+        obj = DataValidationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
